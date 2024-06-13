bb51054302d76c2f8b14cb65c01e040a4e2d148d
0x-alx <alx.charbo@gmail.com>
2024-06-13 11:16:38 +0200

    fix css variables

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,11 +5,12 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-	"inline-flex  items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50  font-bold",
+	"inline-flex  items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 font-bold",
 	{
 		variants: {
 			variant: {
-				default: "bg-primary text-white hover:bg-primary",
+				default:
+					"bg-primary text-white hover:opacity-80 active:scale-95 transition-all",
 				destructive:
 					"bg-destructive text-destructive-foreground hover:bg-destructive/90",
 				outline:
