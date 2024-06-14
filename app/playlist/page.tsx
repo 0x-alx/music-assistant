9d5b7808c70ca6419a0c1ebcbc23baaaba35a0d4
0x-alx <alx.charbo@gmail.com>
2024-06-14 12:18:43 +0200

    Add custom image cover to playlist

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -12,6 +12,7 @@ import {
 	addTrackToPlaylist,
 	createPlaylist,
 	getSpotifyProfile,
+	addCustomCoverImageToPlaylist,
 } from "@/utils/hooks/spotifyHooks";
 import { toast } from "sonner";
 import { CheckCircleIcon, Plus } from "lucide-react";
@@ -52,6 +53,11 @@ export default function Home() {
 			name: playlistName,
 		});
 
+		const image = await addCustomCoverImageToPlaylist({
+			accessToken: session?.access_token!,
+			playlistId: playlist.id,
+		});
+
 		const trackListIds = searchResult.map((track: any) => track.uri);
 
 		await addTrackToPlaylist({
@@ -82,11 +88,11 @@ export default function Home() {
 	return (
 		<div className='flex w-full min-h-screen flex-col items-center justify-between px-4 py-24 lg:px-48 lg:py-24 '>
 			<div
-				className='relative w-full h-full max-h-[80vh] lg:max-h-[800px] bg-gradient-to-b from-blue-700 to-neutral-900 rounded-xl overflow-scroll'
+				className='relative w-full h-full max-h-[80vh] lg:max-h-[800px] bg-gradient-to-b from-[#F1EFE7] to-neutral-900 rounded-xl overflow-scroll'
 				ref={scrollContainerRef}
 			>
 				{showTopBar && (
-					<div className='sticky top-0 w-full z-10 flex items-center justify-between p-4 gap-4 bg-blue-700'>
+					<div className='sticky top-0 w-full z-10 flex items-center justify-between p-4 gap-4 bg-[#F1EFE7]'>
 						<div className='flex items-center gap-4'>
 							<div className='size-8 bg-black rounded-md'></div>
 							<h2 className='text-white text-xl lg:text-3xl font-bold'>
@@ -135,7 +141,14 @@ export default function Home() {
 					</div>
 				)}
 				<div className='w-full h-fit lg:h-1/3 lg:max-h-[400px] flex items-center lg:items-end p-4 gap-4 lg:gap-6 bg-[url("/waves-bg.webp")] bg-cover bg-center bg-no-repeat bg-blend-overlay bg-[#00000090]'>
-					<div className='size-20 min-w-20 lg:size-40 lg:min-w-40 bg-black rounded-md'></div>
+					<div className='size-20 min-w-20 lg:size-40 lg:min-w-40 rounded-md overflow-hidden'>
+						<Image
+							src='/playlist-thumbnail.jpeg'
+							alt='Playlist Thumbnail'
+							width={160}
+							height={160}
+						/>
+					</div>
 					<div className='flex w-full flex-col gap-2'>
 						{successfullAdd ? (
 							<div className='h-9 px-4 py-2 font-bold max-w-[200px] hidden lg:flex items-center justify-center bg-transparent border rounded-sm border-primary text-primary gap-2'>
